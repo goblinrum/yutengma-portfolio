55607295922d24e04c9839f063530d889efc4fc0
Ryan Ma <ryanmaawesome2@gmail.com>
2022-12-31 20:06:59 -0600

    pocketbase and site changes

--- components/Container.tsx
+++ components/Container.tsx
@@ -50,8 +50,8 @@ export default function Container(props) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
-        <link rel="canonical" href={`https://leerob.io${router.asPath}`} />
+        <meta property="og:url" content={`https://yutengma.me${router.asPath}`} />
+        <link rel="canonical" href={`https://yutengma.me${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Ryan Ma" />
         <meta property="og:description" content={meta.description} />
